30bbe83af0a448ed5174cff93a433bf1614596a0
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-12-04 17:45:20 +0100

    add external routing for endpoints

---
+++ routes/testimonials.routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+const db = require('../db');
+
+
+router.route('/testimonials').get((req, res) => {
+    res.json(db.testimonials);
+    console.log('db.test', db.testimonials)
+  });
+  
+router.route('/testimonials/random').get((req, res) => {
+  const randomEl = Math.floor(Math.random()*db.testimonials.length);
+  res.json(db.testimonials[randomEl]);
+});
+  
+router.route('/testimonials').post((req, res) => {
+  const { author, text } = req.body;
+  db.testimonials.push(  { id: uuidv4(), author: author, text: text });
+  res.json({ massage: 'OK'} );
+  });
+  
+router.route('/testimonials/:id').put((req, res) => {
+  const { author, text } = req.body;
+  const objOfArray = db.testimonials[req.params.id-1];
+  objOfArray.author = author;
+  objOfArray.text = text;
+  res.json({ massage: 'OK'} );
+});
+
+router.route('/testimonials/:id').delete((req, res) => {
+  db.testimonials.splice(req.params.id-1, 1);
+  res.json({ massage: 'OK'} );
+});
+
+router.route('/testimonials/:id').get((req, res) => {
+  res.json(db.testimonials[req.params.id-1]);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,50 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
+const db = require('./db')
 const app = express();
 
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
 
+//import routes
+const testimonials = require('./routes/testimonials.routes');
   
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors()); //cors stoi za funkcjonalnością wykonywania żądań AJAX-owych. Możemy np. ustawić, że nasze API pozwala na połączenie tylko i wyłączne z konkretnej domeny 
 
-
-app.get('/testimonials', (req, res) => {
-  res.json(db);
-});
-
-app.get('/testimonials/random', (req, res) => {
-  const randomEl = Math.floor(Math.random()*db.length);
-  res.json(db[randomEl]);
-});
-
-app.post('/testimonials', (req, res) => {
-  const { author, text } = req.body;
-  db.push(  { id: uuidv4(), author: author, text: text });
-  res.json({ massage: 'OK'} );
-  });
-
-app.put('/testimonials/:id', (req, res) => {
-    const { author, text } = req.body;
-    const objOfArray = db[req.params.id-1];
-    objOfArray.author = author;
-    objOfArray.text = text;
-    res.json({ massage: 'OK'} );
-});
-
-app.delete('/testimonials/:id', (req, res) => {
-  db.splice(req.params.id-1, 1);
-  res.json({ massage: 'OK'} );
-});
-
-app.get('/testimonials/:id', (req, res) => {
-  res.json(db[req.params.id-1]);
-});
+//use routes
+app.use('/', testimonials); 
 
 app.use((req, res) => {
     res.json({ message: 'Not found...' });
