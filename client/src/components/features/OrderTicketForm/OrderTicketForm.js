34fa0fca1432ffac48a41aedf53b5bfe87e811ba
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-12-05 09:57:58 +0100

    add async for loadSeats to be fired after addSeat()

--- client/src/components/features/OrderTicketForm/OrderTicketForm.js
+++ client/src/components/features/OrderTicketForm/OrderTicketForm.js
@@ -40,11 +40,13 @@ class OrderTicketForm extends React.Component {
   submitForm = async (e) => {
     const { order } = this.state;
     const { addSeat } = this.props;
+    const { loadSeats } = this.props;
 
     e.preventDefault();
 
     if(order.client && order.email && order.day && order.seat) {
-      addSeat(order);
+      await addSeat(order);
+      loadSeats()
       this.setState({ 
         order: {
           client: '',
